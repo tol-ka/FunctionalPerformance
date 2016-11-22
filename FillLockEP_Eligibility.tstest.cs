a46b93d54c72f327d843f3bf0f39fb05c6d90ac3
yacov <jakoff@gmail.com>
2016-11-22 23:23:42 +0200

    Added EP Eligibility

---
+++ FillLockEP_Eligibility.tstest.cs
@@ -0,0 +1,86 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace PerformanceTesting
+{
+
+    public class FillLockEPEligibility : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"SetPlantoEP")]
+        public void SetPlantoEP()
+        {
+                     Utility.plan = @"EP";
+        }
+    
+        [CodedStep(@"SaveCurrentBuild")]
+        public void SaveCurrentBuild1()
+        {
+                                                object myData = GetExtractedValue("currentBuild");
+                        var _build = myData.ToString();
+                        //removing all but numbers in build
+                       // String build = Regex.Replace(_build, @"\D+", String.Empty);
+                        String build = new String(_build.Where(Char.IsDigit).ToArray());
+                                                Utility.currentBuild = build;
+        }
+    
+        [CodedStep(@"Set event Name")]
+        public void setEventName()
+        {
+                        Utility.eventName = @"EP Eligibility Meeting";
+        }
+    
+    
+    
+        [CodedStep(@"Set event Group Name")]
+        public void setEventGroupName()
+        {
+                        Utility.eventGroupName = @"EP Eligibility Meeting and EP Initial Meeting";
+        }
+        
+        [CodedStep(@"Set event Type")]
+        public void setEventType()
+        {
+            Utility.eventType = @"Eligibility";
+        }
+    }
+}
---
+++ OpenSaveCount/OpenSaveCountEP_Eligibility.tstest.cs
@@ -0,0 +1,64 @@
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace PerformanceTesting
+{
+
+    public class OpenSaveCountEP_Eligibility : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"ChooseAction")]
+        public void OpenSaveCount_ChooseAction()
+        {
+                        if(Utility.saveflag == "normal"){
+                            this.ExecuteTest("verify\\SaveFormAndCount.tstest");
+                        }
+                        else if(Utility.saveflag == "nosaveButOther"){
+                            this.ExecuteTest("verify\\ActFormAndCount.tstest");
+                        }
+                         else if(Utility.saveflag == "nosaveNoOther"){
+                            this.ExecuteTest("verify\\WriteToExcel.tstest");
+                        }
+                         Utility.saveflag = "normal";
+        }
+    }
+}
